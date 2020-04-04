96d993eebe5c22a30f28357e999f09f4759e97c0
matthew <leematt5@uw.edu>
2020-04-04 11:08:15 -0700

    includes uxsubmit logic

--- HelloWorld/MainWindow.xaml.cs
+++ HelloWorld/MainWindow.xaml.cs
@@ -23,6 +23,38 @@ namespace HelloWorld
         public MainWindow()
         {
             InitializeComponent();
+            uxSubmit.IsEnabled = false;
+        }
+
+
+
+        private void uxSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Submitting password:" + uxPassword.Text);
+        }
+
+        private void uxName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (uxName.Text.Length == 0)
+            {
+                uxSubmit.IsEnabled = false;
+            }
+            else if (uxPassword.Text.Length > 0)
+            {
+                uxSubmit.IsEnabled = true;
+            }
+        }
+
+        private void uxPassword_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (uxPassword.Text.Length == 0)
+            {
+                uxSubmit.IsEnabled = false;
+            }
+            else if (uxName.Text.Length > 0)
+            {
+                uxSubmit.IsEnabled = true;
+            }
         }
     }
 }
